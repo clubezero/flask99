b1b29612f0795faec16cf25c5f7781d48a85a7e7
clubezero <clubezero99@gmail.com>
2026-02-11 08:57:46 +0100

    seundo comitd

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from app import aplication
-from flask import render_template
+from flask import render_template, url_for
 
 @aplication.route('/')
 
 def home():
 	return render_template('index.html')
+
+@aplication.route('/elencos')
+def page():
+	return "Nova pagina"
